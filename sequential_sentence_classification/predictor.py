87c99d81791d7ce1fd055376c89d34497f595d56
karthic-tamu <79726805+karthic-tamu@users.noreply.github.com>
2021-12-20 11:24:14 -0600

    Fixed few bugs in the predictor

--- sequential_sentence_classification/predictor.py
+++ sequential_sentence_classification/predictor.py
@@ -15,13 +15,15 @@ class SeqClassificationPredictor(Predictor):
         pred_labels = []
         sentences = json_dict['sentences']
         paper_id = json_dict['abstract_id']
+        self._dataset_reader.predict = True
         for sentences_loop, _, _, _ in  \
             self._dataset_reader.enforce_max_sent_per_example(sentences):
-            instance = self._dataset_reader.text_to_instance(abstract_id=0, sentences=sentences_loop)
+            instance = self._dataset_reader.text_to_instance(sentences=sentences)
             output = self._model.forward_on_instance(instance)
             idx = output['action_probs'].argmax(axis=1).tolist()
             labels = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
-            pred_labels.extend(labels)
+            pred_labels.extend(labels)        
+        pred_labels = pred_labels[:len(sentences)]        
         assert len(pred_labels) == len(sentences)
         preds = list(zip(sentences, pred_labels))
         return paper_id, preds
